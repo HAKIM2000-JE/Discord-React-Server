f8ef6cba81608ba201ba5e0d7dfcf8f5d4475ac7
Jebabra Abdelhakim <59029617+HAKIM2000-JE@users.noreply.github.com>
2021-05-13 15:58:28 +0000

    Create Chat.js

---
+++ src/Chat.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+import './chat.css'
+import ChatHeader from './ChatHeader'
+
+import AddCircleIcon from '@material-ui/icons/AddCircle'
+import GifIcon from "@material-ui/icons/Gif"
+import CardGiftcardIcon from "@material-ui/icons/CardGiftcard"
+import EmojiEmotionsIcon from "@material-ui/icons/EmojiEmotions"
+import Message from './Message'
+
+
+function Chat() {
+    return (
+        <div className="chat">
+           <ChatHeader/>
+
+           <div className="chat__messages">
+                <Message />
+                <Message />
+                <Message />
+                <Message/>
+           </div>
+
+           <div className="chat__input">
+                <AddCircleIcon fontSize="large" />
+
+
+                <form>
+                    <input type="text" placeholder="message message message message"/>
+                   <button type="submit" className="chat__inputbtn">Send Message</button>
+                </form>
+
+
+
+                <div className="chat__inputIcons">
+                  <CardGiftcardIcon  fontSize="large"/>
+                  <GifIcon fontSize="large"/>
+                  <EmojiEmotionsIcon fontSize="large"/>
+                </div>
+           </div>
+            
+        </div>
+    )
+}
+
+export default Chat
